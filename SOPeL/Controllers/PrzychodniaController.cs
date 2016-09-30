a2f8c7c0adacfc81e6b3094000e3fbb19213aded
MeHow91PL <19owen91@gmail.com>
2016-10-01 00:09:29 +0200

    Dodano przycisk opcji w terminarzu, wysyłanie opcji za pomocą AJAX, okno opcji

--- SOPeL/Controllers/PrzychodniaController.cs
+++ SOPeL/Controllers/PrzychodniaController.cs
@@ -3,22 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PostgresObsuga;
 
 namespace SOPeL.Controllers
 {
-	public class PrzychodniaController : Controller
-	{
-		//
-		// GET: /Przychodnia/
-		public ActionResult Index()
-		{
-			return View();
-		}
+    public class PrzychodniaController : Controller
+    {
+        //
+        // GET: /Przychodnia/
+        public ActionResult Index()
+        {
+            return View();
+        }
 
-		public ActionResult pobierzModulAdmin(string wybranyModul)
-		{
-			string modul = wybranyModul;
+        public ActionResult pobierzModulAdmin(string wybranyModul)
+        {
+            string modul = wybranyModul;
             return RedirectToAction("Index", modul);
-		}
-	}
+        }
+
+        public bool zapiszOpcjeTerminarza()
+        {
+            try
+            {
+                Database.otworzPolaczenie("localhost", "postgres", "postgres", "SOPeL");
+
+                Database.zamknijPolaczenie();
+
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
 }
