1520592f881e746b01d01048cc6304239f3b62ab
pc <pc@PC-KOMPUTER>
2016-10-13 17:02:07 +0200

    Dodaje w przychodni poczeklanie ktora bedzie prowadzic do wiyty.

---
+++ SOPeL/Controllers/PoczekalniaController.cs
@@ -0,0 +1,40 @@
+﻿using SOPeL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PostgresObsuga;
+using Npgsql;
+
+namespace SOPeL.Controllers
+{
+    public class PoczekalniaController : Controller
+    {
+        // GET: Poczekalnia
+        public ActionResult Index()
+        {
+            Database.otworzPolaczenie("serwer1518407.home.pl", "18292517_0000002", "Sopel2016", "18292517_0000002");
+            ViewBag.Pracownicy = GetListaPracownikow("Select * from pracownicy");
+            return View("~/Views/Przychodnia/Poczekalnia/Index.cshtml");
+            Database.zamknijPolaczenie();
+        }
+
+
+        internal List<Pracownik> GetListaPracownikow(string zapytanie)
+        {
+            List<Pracownik> pracownicy = new List<Pracownik>();
+
+            NpgsqlDataReader dr = Database.wykonajZapytanieDQL(zapytanie);
+
+            while (dr.Read())
+            {
+                pracownicy.Add(new Pracownik() { Id = (int)dr["id"], Imie = dr["imie"].ToString(), Nazwisko = dr["nazwisko"].ToString(), Specjalizacja = dr["specjalizacja"].ToString() });
+            }
+
+            dr.Close();
+
+            return pracownicy;
+        }
+    }
+}
---
+++ SOPeL/Controllers/WizytaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SOPeL.Controllers
+{
+    public class WizytaController : Controller
+    {
+        // GET: Wizyta
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
