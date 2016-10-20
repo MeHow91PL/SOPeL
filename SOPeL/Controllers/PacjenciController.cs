bb65af9eb815dd3f3cb74f3c9efab42c9e930520
Krzysztof <Krzysztof@DESKTOP-E5GOAAM>
2016-10-20 14:07:41 +0200

    W sekcji pacjenci zaczytywana jest lista pacjentów. ALe nie można ich jeszcze wyszukiwac ani sortowac

--- SOPeL/Controllers/PacjenciController.cs
+++ SOPeL/Controllers/PacjenciController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PostgresObsuga;
+using Npgsql;
+using SOPeL.Models;
 
 namespace SOPeL.Controllers
 {
@@ -11,7 +14,27 @@ namespace SOPeL.Controllers
         // GET: Pacjenci
         public ActionResult Index()
         {
+            //return View("~/Views/Przychodnia/Pacjenci/Index.cshtml");
+            Database.otworzPolaczenie("serwer1518407.home.pl", "18292517_0000002", "Sopel2016", "18292517_0000002");
+            ViewBag.Pacjenci = GetListaPacjentow("Select * from pacjenci");
+            Database.zamknijPolaczenie();
             return View("~/Views/Przychodnia/Pacjenci/Index.cshtml");
+        }
+
+        internal List<Pacjent> GetListaPacjentow(string zapytanie)
+        {
+            List<Pacjent> pacjenci = new List<Pacjent>();
+
+            NpgsqlDataReader dr = Database.wykonajZapytanieDQL(zapytanie);
+
+            while (dr.Read())
+            {
+                pacjenci.Add(new Pacjent() { Imie = dr["imie"].ToString(), Nazwisko = dr["nazwisko"].ToString(), Pesel = dr["pesel"].ToString() });
+            }
+
+            dr.Close();
+
+            return pacjenci;
         }
     }
 }
