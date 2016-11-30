5ba226e943e62f07c0feb605fc74e41132ccd943
Filip <filip.kopczynski@gmail.com>
2016-11-30 20:30:21 +0100

    Pacjenci - działa dodawania nowego pacjenta do bazy

--- SOPeL/Controllers/PacjenciController.cs
+++ SOPeL/Controllers/PacjenciController.cs
@@ -15,10 +15,7 @@ namespace SOPeL.Controllers
         // GET: Pacjenci
         public ActionResult Index()
         {
-            //return View("~/Views/Przychodnia/Pacjenci/Index.cshtml");
-            //Database.otworzPolaczenie("serwer1518407.home.pl", "18292517_0000002", "Sopel2016", "18292517_0000002");
-            //ViewBag.Pacjenci = GetListaPacjentow("Select * from pacjenci");
-            //Database.zamknijPolaczenie();
+            
             List<Pacjent> pacjenci = db.Pacjenci.ToList();
             return View("~/Views/Przychodnia/Pacjenci/Index.cshtml",pacjenci);
         }
@@ -27,16 +24,26 @@ namespace SOPeL.Controllers
         {
             List<Pacjent> pacjenci = new List<Pacjent>();
 
-            //NpgsqlDataReader dr = Database.wykonajZapytanieDQL(zapytanie);
-
-            //while (dr.Read())
-            //{
-            //    pacjenci.Add(new Pacjent() { Imie = dr["imie"].ToString(), Nazwisko = dr["nazwisko"].ToString(), Pesel = dr["pesel"].ToString() });
-            //}
-
-            //dr.Close();
-
             return pacjenci;
+        }
+
+        public PartialViewResult dodajPacjenta()
+        {
+            Pacjent pacjent = new Pacjent();
+            return PartialView("_KartaPacjenta",pacjent);
+        }
+
+
+
+        public ActionResult ZapiszDodajPacjenta([Bind(Include = "Imie,Nazwisko,Pesel,KodPocztowy,Miasto,Ulica,Telefon,Email,Plec")] Pacjent pacjent)
+        {
+           
+                    db.Pacjenci.Add(pacjent);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+              
+
+            
         }
     }
 }
--- SOPeL/Scripts/Pacjenci.js
+++ SOPeL/Scripts/Pacjenci.js
@@ -1,10 +1,24 @@
 ﻿/// <reference path="jquery-3.1.1.js" />
 
 
-//$(document).ready(function () {
-//    alert("alert pacjenci");
-//});
+$(document).ready(function () {
+    var $dodajPacjenta = $("#dodajPacjenta");
+    var $kartaRezerwacjiWizytyKontener = $("#kartaRezerwacjiWizytyKontener");
 
-$(".dodaj-pacjenta").click(function () {
-    alert("Pojedyncze klikniecie klikniecie");
+    $dodajPacjenta.click(function () {
+        //alert("Pojedyncze klikniecie klikniecie");
+
+        $kartaRezerwacjiWizytyKontener.css("display", "flex");
+        $.ajax({
+            url: '/Pacjenci/dodajPacjenta',
+            type: 'POST',
+            success: function (response) {
+                $kartaRezerwacjiWizytyKontener.html(response);
+            },
+            error: function () {
+                alert("Error");
+            }
+        });
+    });
 });
+
