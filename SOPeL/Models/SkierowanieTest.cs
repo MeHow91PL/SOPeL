694d6b0df4dd8a14163de6fab67ec3f3ad392b67
Michał <19owen91@gmail.com>
2017-01-12 14:38:09 +0100

    testowanie drukowania

---
+++ SOPeL/Models/SkierowanieTest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SOPeL.Models
+{
+    public class SkierowanieTest
+    {
+        public string data { get; set; }
+        public string poradnia { get; set; }
+    }
+}
